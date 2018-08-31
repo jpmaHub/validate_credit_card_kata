635d0fd368544038c99743a6c7b5288a3b8b7c27
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2018-08-31 13:14:23 +0100

    Refactor test

--- spec/validate_credit_card_spec.rb
+++ spec/validate_credit_card_spec.rb
@@ -12,7 +12,7 @@ describe ValidateCreditCard do
   end
 
   it 'can raise Argument error when more than 16 digits passed' do
-    expect { described_class.new.validate(12_345_678_912_345_673) }.to raise_error(ArgumentError)
+    expect { described_class.new.validate(12345678912345673) }.to raise_error(ArgumentError)
   end
 
   it 'can double the digit from scanning for even numbers ' do
@@ -20,7 +20,7 @@ describe ValidateCreditCard do
   end
 
   it 'can double the digit from scanning for odd numbers ' do
-    expect(described_class.new.sum_doubled_digit(89_190)).to eq(27)
+    expect(described_class.new.sum_doubled_digit(89190)).to eq(27)
   end
 
   it 'can return true when 2121 valid number passed' do
